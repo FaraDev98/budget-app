023ee4f466117d409403bc17c22aa95f75c50fe1
Diego Faravelli Ext <diego.faravelli.ext@ticketone.it>
2025-04-24 18:28:57 +0200

    feat: Remove unused Data Connect configuration and generated SDK files

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'app-rendicontazione-spese',
-  location: 'us-central1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,7 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'app-rendicontazione-spese',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
